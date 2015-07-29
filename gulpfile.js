3e0a05ce074f3e73998260a05f339be89f3c3280
Zeb Pykosz <zeb.apy@gmail.com>
2015-07-29 15:38:47 -0400

    fix indentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var sass        = require('gulp-sass');
+var reload = browserSync.reload;
+var sass = require('gulp-sass');
  
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
     //watch files
     var files = [
-    './style.css',
-    './*.php'
+        './style.css',
+        './*.php'
     ];
  
     //initialize browsersync
     browserSync.init(files, {
     //browsersync with a php server
-    proxy: "localhost:8888/NER/",
-    notify: false
+        proxy: "localhost:8888/NER/",
+        notify: false
     });
 });
  
