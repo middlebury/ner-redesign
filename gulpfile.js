a4d244c2b13869675fed43ab3a11e5059f1e16e3
Zeb Pykosz <zeb.apy@gmail.com>
2015-07-29 16:58:13 -0400

    write external source maps

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('sass', function () {
     return gulp.src('sass/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./'))
         .pipe(reload({stream:true}));
 });
