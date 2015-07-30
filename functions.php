423c3afe8ea37c1633a43cca1701f6134c6ee237
Zeb Pykosz <zeb.apy@gmail.com>
2015-07-30 15:55:07 -0400

    remove 'archives for' prefixes

--- functions.php
+++ functions.php
@@ -169,13 +169,13 @@ function sp_breadcrumb_args( $args ) {
 	$args['heirarchial_categories'] = true; // Genesis 1.5 and later
 	$args['display'] = true;
 	$args['labels']['prefix'] = '';
-	$args['labels']['author'] = 'Archives for ';
-	$args['labels']['category'] = 'Archives for '; // Genesis 1.6 and later
-	$args['labels']['tag'] = 'Archives for ';
-	$args['labels']['date'] = 'Archives for ';
+	$args['labels']['author'] = '';
+	$args['labels']['category'] = ''; // Genesis 1.6 and later
+	$args['labels']['tag'] = '';
+	$args['labels']['date'] = '';
 	$args['labels']['search'] = 'Search for ';
-	$args['labels']['tax'] = 'Archives for ';
-	$args['labels']['post_type'] = 'Archives for ';
+	$args['labels']['tax'] = '';
+	$args['labels']['post_type'] = '';
 	$args['labels']['404'] = 'Not found: '; // Genesis 1.5 and later
 return $args;
 }
