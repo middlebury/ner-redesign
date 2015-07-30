9d6db19fcb762dd07173d401aaab8a0f81f8adc6
Zeb Pykosz <zeb.apy@gmail.com>
2015-07-30 10:27:22 -0400

    add combine media queries

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var cmq = require('gulp-combine-mq');
  
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
@@ -26,6 +27,7 @@ gulp.task('sass', function () {
     return gulp.src('sass/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(cmq())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./'))
         .pipe(reload({stream:true}));
