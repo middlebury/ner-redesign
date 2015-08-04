49c006b6c2f8deed7722b9ecf6ad6e1366cdb8b8
Zeb Pykosz <zeb.apy@gmail.com>
2015-08-04 14:12:41 -0400

    remove backstretch plugin

--- js/backstretch-set.js
+++ js/backstretch-set.js
@@ -1,3 +1,34 @@
-jQuery(document).ready(function($) {
-	$("body").backstretch([BackStretchImg.src],{duration:3000,fade:750});
-});
+// Global var for background image BackStretchImg.src
+
+// jQuery(document).ready(function($) {
+
+//     var imgSrc = BackStretchImg.src;
+
+//     var $bgContainer = $('<div></div>');
+
+//     $bgContainer.addClass('ner-bg-image');
+
+//     $bgContainer.css({
+//         backgroundImage: "url('" + imgSrc + "')",
+//         backgroundSize: 'cover',
+//         backgroundRepeat: 'no-repeat',
+//         backgroundPosition: '0 0',
+//         position: 'fixed',
+//         width: '100%',
+//         height: '100%',
+//         top: 0,
+//         left: 0,
+//         opacity: 0,
+//         zIndex: -1,
+//         transition: 'opacity 2s'
+//     });
+
+//     $('body').append($bgContainer);
+
+//     $(window).load(function() {
+//         $bgContainer.css({
+//             opacity: .6
+//         });
+//     });
+
+// });
